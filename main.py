f3ce9cc247df58fa1c3554bd539fcb7a84fef2c3
Mehmet-Mazi <m.mazi1905@gmail.com>
2025-07-19 14:05:33 +0100

    book report prod ready

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
-from stats import get_word_count, repeat_char_count
+from stats import get_word_count, repeat_char_count, get_sort_repeat_chars
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    # print(text)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
     word_count = get_word_count(text)
-    print(f"{word_count} words found in the document")
     repeating_chars = repeat_char_count(text)
-    print(repeating_chars)
+    sorted_chars = get_sort_repeat_chars(repeating_chars)
+    print_report(book_path, word_count, sorted_chars)
+
+def print_report(book_path, word_count, sorted_chars_list):
+
+    print("============ BOOKBOT ============")
+
+    print(f"Analyzing book found at {book_path}")
+
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+
+    print("--------- Character Count -------")
+    for char in sorted_chars_list:
+        if char["char"].isalpha():
+            print(f"{char['char']}: {char['num']}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,15 @@ def repeat_char_count(text):
 
 def get_word_count(text):
     return len(text.split())
+
+def sort_on(items):
+    return items["num"]
+
+def get_sort_repeat_chars(char_dict):
+    sort_list = []
+    for key in char_dict:
+        sort_list.append({"char":key, "num": char_dict[key]})
+        
+    sort_list.sort(reverse=True, key=sort_on)
+    return sort_list
+
